d1bb963eabe8040d9659d2135177696881ebbf1e
edgardoeg <edgardoeg@yahoo.com>
2025-06-10 23:26:02 -0600

    Ejercicio casi terminado

---
+++ CRUMVVM/ViewModels/AddEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CRUMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUMVVM.ViewModels
+{
+    public class AddEditViewModel
+    {
+        public AddEditViewModel() { }
+
+        public AddEditViewModel(Empleado Empleado) { }
+    }
+}
--- CRUMVVM/Views/AddEditView.xaml.cs
+++ CRUMVVM/Views/AddEditView.xaml.cs
@@ -1,9 +1,21 @@
+using CRUMVVM.Models;
+using CRUMVVM.ViewModels;
+
 namespace CRUMVVM.Views;
 
 public partial class AddEditView : ContentPage
 {
+	private AddEditViewModel viewModel;
 	public AddEditView()
 	{
 		InitializeComponent();
-	}
+        viewModel = new AddEditViewModel();
+        this.BindingContext = viewModel;
+    }
+    public AddEditView(Empleado Empleado)
+    {
+        InitializeComponent();
+        viewModel = new AddEditViewModel(Empleado);
+        this.BindingContext = viewModel;
+    }
 }
