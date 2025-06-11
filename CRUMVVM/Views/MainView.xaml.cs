2eb6b93e9e3abb2bb7f681c8aba37afbb446140d
edgardoeg <edgardoeg@yahoo.com>
2025-06-10 23:58:51 -0600

    Queda funcionando el boton de agregar y cambia al formulario

--- CRUMVVM/Views/MainView.xaml.cs
+++ CRUMVVM/Views/MainView.xaml.cs
@@ -1,9 +1,15 @@
+using CRUMVVM.ViewModels;
+
 namespace CRUMVVM.Views;
 
 public partial class MainView : ContentPage
 {
+	private MainViewModel viewModel;
 	public MainView()
 	{
+		
 		InitializeComponent();
-	}
+		viewModel = new MainViewModel();
+		this.BindingContext = viewModel;
+    }
 }
