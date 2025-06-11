a786a45c36a3e81ef3f4ea7235d172785a61843d
edgardoeg <edgardoeg@yahoo.com>
2025-06-10 23:25:46 -0600

    terminando de colocar el codigo en el MainViewModel

---
+++ CRUMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CRUMVVM.Models;
+using CRUMVVM.Services;
+using CRUMVVM.Views;
+using System.Collections.ObjectModel;
+
+namespace CRUMVVM.ViewModels
+{
+    public partial class MainViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Empleado> empleadoCollection = new ObservableCollection<Empleado>();
+
+        private EmpleadoService _Service;
+
+        public MainViewModel()
+        {
+            _Service = new EmpleadoService();
+        }
+
+        private void Alerta(string Titulo, string Mensaje)
+
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+        }
+
+        public void GetAll()
+        { 
+        
+            var getAll = _Service.GetAll();
+
+            if (getAll.Count > 0)
+            {
+                EmpleadoCollection.Clear();
+                foreach (var empleado in getAll)
+                {
+                    EmpleadoCollection.Add(empleado);
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async Task GoToAddEditView()
+        {
+            await App.Current!.MainPage!.Navigation.PushAsync(new AddEditView());
+        }
+
+    }
+}
